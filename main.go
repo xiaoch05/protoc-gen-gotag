7b28ab547c5ee69e75ecd4d65479bd9652bad144
xiaocheng <xiaoch2010@gmail.com>
2020-07-27 17:13:44 +0800

    test

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	pgs "github.com/lyft/protoc-gen-star"
 	pgsgo "github.com/lyft/protoc-gen-star/lang/go"
+	"fmt"
 
 	"github.com/amsokol/protoc-gen-gotag/module"
 )
 
 func main() {
+	fmt.Println("start protoc-gen-gotag...")
 	pgs.Init(pgs.DebugEnv("DEBUG")).
 		RegisterModule(module.New()).
 		RegisterPostProcessor(pgsgo.GoFmt()).
